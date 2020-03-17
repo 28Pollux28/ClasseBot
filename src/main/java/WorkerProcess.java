db7c9b7ab2586991741268901cef984c609afcb3
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 00:29:50 +0100

    Update WorkerProcess.java

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -2,12 +2,12 @@ public class WorkerProcess
 {
     public static void main(String[] args)
     {
-        while(true) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Worker process woke up ("+(i+1)+"/10");
             try {
                 Thread.sleep(5000);
             } catch(InterruptedException e) {}
-
-            System.out.println("Worker process woke up");
         }
+        System.out.println("Worker process finished !");
     }
 }

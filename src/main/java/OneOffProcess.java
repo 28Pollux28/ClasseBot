8aa9501c1350157c7de209402655c566ce6b2d07
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-17 23:51:20 +0100

    Create OneOffProcess.java

---
+++ src/main/java/OneOffProcess.java
@@ -0,0 +1,8 @@
+//https://devcenter.heroku.com/articles/run-non-web-java-processes-on-heroku#src-main-java-oneoffprocess-java
+public class OneOffProcess
+{
+    public static void main(String[] args)
+    {
+        System.out.println("OneOffProcess executed.");
+    }
+}

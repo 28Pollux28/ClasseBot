72a899fbcb6bd9b996bec480a44defcb18c0d9fb
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 16:16:17 +0100

    Add files via upload

---
+++ src/main/java/MessageListener.java
@@ -0,0 +1,28 @@
+public class MessageListener extends ListenerAdapter
+{
+    public static void main(String[] args)
+            throws LoginException
+    {
+        JDA jda = new JDABuilder(System.getenv("TOKEN")).build();
+        //You can also add event listeners to the already built JDA instance
+        // Note that some events may not be received if the listener is added after calling build()
+        // This includes events such as the ReadyEvent
+        jda.addEventListeners(new MessageListener());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContentDisplay());
+        }
+    }
+}

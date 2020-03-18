42619e31bfbcc46bbfd957fd0590d280c2b9d890
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 15:57:35 +0100

    Add files via upload

---
+++ src/main/java/JDAExample.java
@@ -0,0 +1,76 @@
+public class ReadyListener implements EventListener
+{
+    public static void main(String[] args)
+            throws LoginException, InterruptedException
+    {
+        // Note: It is important to register your ReadyListener before building
+        JDA jda = new JDABuilder(System.getenv("TOKEN"))
+            .addEventListeners(new ReadyListener())
+            .build();
+
+        // optionally block until JDA is ready
+        jda.awaitReady();
+    }
+
+    @Override
+    public void onEvent(GenericEvent event)
+    {
+        if (event instanceof ReadyEvent)
+            System.out.println("API is ready!");
+    }
+}
+
+public class MessageListener extends ListenerAdapter
+{
+    public static void main(String[] args)
+            throws LoginException
+    {
+        JDA jda = new JDABuilder(System.getenv("TOKEN")).build();
+        //You can also add event listeners to the already built JDA instance
+        // Note that some events may not be received if the listener is added after calling build()
+        // This includes events such as the ReadyEvent
+        jda.addEventListeners(new MessageListener());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                        event.getMessage().getContentDisplay());
+        }
+    }
+}
+
+public class Bot extends ListenerAdapter
+{
+    public static void main(String[] args) throws LoginException
+    {
+        new JDABuilder(args[0])
+            .addEventListeners(new Bot())
+            .setActivity(Activity.playing("Type !ping"))
+            .build();
+    }
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            long time = System.currentTimeMillis();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                   .queue(response /* => Message */ => {
+                       response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                   });
+        }
+    }
+}

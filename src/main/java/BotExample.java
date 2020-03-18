85b2aae1b6f88571eab64fedb46af3f501950a3f
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 19:16:07 +0100

    Add files via upload

--- src/main/java/BotExample.java
+++ src/main/java/BotExample.java
@@ -1,9 +1,11 @@
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.hooks;
-import net.dv8tion.jda.api.events.message;
-import net.dv8tion.jda.api.entities;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 
 import javax.security.auth.login.LoginException;

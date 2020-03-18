ed3ef06941a5790c9f9ef2d2803707f0a6089be0
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 09:49:11 +0100

    Update ClassBotTest.java

--- src/main/java/ClassBotTest.java
+++ src/main/java/ClassBotTest.java
@@ -15,10 +15,10 @@ import net.dv8tion.jda.api.entities.Member;
 
 public class ClassBotTest implements Runnable {
 	//static public ArrayList<Classe> classes = new ArrayList<Classe>();
-	private final JDA jda;
-	private final CommandMap commandMap = new CommandMap(this);
+	/*private final JDA jda;
+	private final CommandMap commandMap = new CommandMap(this);*/
 	private boolean running;
-	private final Scanner scanner = new Scanner(System.in);
+	/*private final Scanner scanner = new Scanner(System.in);
 	//static public HashMap<Member,Classe> memberClasses = new HashMap<Member,Classe>();
 	
 	public ClassBotTest() throws LoginException {
@@ -26,7 +26,7 @@ public class ClassBotTest implements Runnable {
 		jda = new JDABuilder(AccountType.BOT).setToken(process.env.TOKEN).build();
 		//jda.addEventListener(new BotListener(commandMap));
 		System.out.println("Started");
-	}
+	}*/
 	
 	/*public static ArrayList<Classe> getClasses(){
 		return classes;
@@ -38,15 +38,15 @@ public class ClassBotTest implements Runnable {
 	@Override
 	public void run() {
 		running = true;
-		while(running) {
+		/*while(running) {
 			if(scanner.hasNextLine()) {
 				//commandMap.commandConsole(scanner.nextLine());
-        scanner.nextLine();//temporaire, retirer quant commandMap sera dispo
+        			scanner.nextLine();//temporaire, retirer quant commandMap sera dispo
 			}
 		}
-		scanner.close();
+		scanner.close();*/
 		System.out.println("Bot Stopped");
-		jda.shutdown();
+		//jda.shutdown();
 		System.exit(0);
 	}
 	
@@ -54,16 +54,16 @@ public class ClassBotTest implements Runnable {
 		this.running = running;
 	}
 	
-	public JDA getJda() {
+	/*public JDA getJda() {
 		return jda;
-	}
+	}*/
 	
 	public static void main(String[] args) {
 		System.out.println("Bot is Starting... (main)");
 		 
 		try {
 			ClassBotTest classBot = new ClassBotTest();
-			new Thread(classBot, "bot").start();
+			new Thread(classBotTest, "botTest").start();
 		} catch (Exception e) {}
 		
 		System.out.println("Hello World !");	

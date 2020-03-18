21256a3e1b24ba3639cb713410233cdefac9a9cc
28Pollux28 <lemaire_valentin28@yahoo.fr>
2020-03-18 21:25:11 +0100

    push with import

--- src/main/java/fr/pollux28/classbot/ClassBot.java
+++ src/main/java/fr/pollux28/classbot/ClassBot.java
@@ -11,6 +11,7 @@ import fr.pollux28.classbot.event.BotListener;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
 
 public class ClassBot implements Runnable{

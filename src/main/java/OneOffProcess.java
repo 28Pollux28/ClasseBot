8d37f61cba5d2c0f9957d6fd1a8f7d352d985be8
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2020-03-18 21:00:02 +0100

    Delete OneOffProcess.java

--- src/main/java/OneOffProcess.java
+++
@@ -1,8 +0,0 @@
-//https://devcenter.heroku.com/articles/run-non-web-java-processes-on-heroku#src-main-java-oneoffprocess-java
-public class OneOffProcess
-{
-    public static void main(String[] args)
-    {
-        System.out.println("OneOffProcess executed.");
-    }
-}

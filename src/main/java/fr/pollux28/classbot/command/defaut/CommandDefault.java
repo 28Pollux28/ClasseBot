17a283b63101b24c70d145f494f3491c60043255
28Pollux28 <lemaire_valentin28@yahoo.fr>
2020-04-11 13:31:53 +0200

    change encoding problems

--- src/main/java/fr/pollux28/classbot/command/defaut/CommandDefault.java
+++ src/main/java/fr/pollux28/classbot/command/defaut/CommandDefault.java
@@ -61,7 +61,7 @@ public class CommandDefault {
 				if(!hasPermission) {
 					fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 					fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez Ãªtre un prof pour dÃ©marrer un cours "+ user.getAsMention(),
+					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez être un prof pour démarrer un cours "+ user.getAsMention(),
 							1,fieldTitle,fieldContent,imgError)).queue();
 					return;
 				}
@@ -77,7 +77,7 @@ public class CommandDefault {
 				if(vc == null) {
 					fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 					fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-					textChannel.sendMessage(messageBuilder("Erreur", "Merci de vous connecter sur un channel vocal pour dÃ©marrer un cours "+ user.getAsMention(),
+					textChannel.sendMessage(messageBuilder("Erreur", "Merci de vous connecter sur un channel vocal pour démarrer un cours "+ user.getAsMention(),
 							1,fieldTitle,fieldContent,imgError)).queue();
 					return;
 				}
@@ -90,15 +90,15 @@ public class CommandDefault {
 						if(cl.getName().equalsIgnoreCase(classe.getName())) {
 							fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 							fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-							classe.getTextChannel().sendMessage(messageBuilder("Erreur", "Une classe nommÃ©e __**"+ classe.getName().toUpperCase()+
-									"**__ existe dÃ©jÃ  ! \n Choississez un autre nom "+ classe.getProf().getAsMention()
+							classe.getTextChannel().sendMessage(messageBuilder("Erreur", "Une classe nommée __**"+ classe.getName().toUpperCase()+
+									"**__ existe déjà ! \n Choississez un autre nom "+ classe.getProf().getAsMention()
 									+" !", 1, fieldTitle, fieldContent,imgError)).queue();
 							return;
 						}
 						if(cl.getProf().getName().equals(classe.getProf().getName())) {
 							fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 							fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention() + " Vous Ãªtes actuellement professeur dans "+ cl.getName()+" sur le salon "
+							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention() + " Vous êtes actuellement professeur dans "+ cl.getName()+" sur le salon "
 									+ cl.getTextChannel().getAsMention()+"\n Fermez cette classe avec /classe stop avant d'en lancer une nouvelle !", 1, fieldTitle, fieldContent,
 									imgError)).queue();
 							return;
@@ -106,7 +106,7 @@ public class CommandDefault {
 						if(cl.getVoiceChannel() == classe.getVoiceChannel()) {
 							fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 							fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention()+" Une classe est dÃ©jÃ  prÃ©sente dans ce salon vocal "
+							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention()+" Une classe est déjà présente dans ce salon vocal "
 									+classe.getVoiceChannel().getName()+ "\n Choisissez un autre salon vocal pour pouvoir lancer votre classe.",1,fieldTitle,fieldContent,
 									imgError)).queue();
 							return;
@@ -114,7 +114,7 @@ public class CommandDefault {
 						if(cl.getTextChannel() == classe.getTextChannel()) {
 							fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 							fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention()+" Une classe est dÃ©jÃ  prÃ©sente dans ce salon textuel "
+							classe.getTextChannel().sendMessage(messageBuilder("Erreur", classe.getProf().getAsMention()+" Une classe est déjà présente dans ce salon textuel "
 									+classe.getTextChannel().getAsMention()+ "\n Choisissez un autre salon textuel pour pouvoir lancer votre classe.",1,fieldTitle,fieldContent,
 									imgError)).queue();
 							return;
@@ -129,26 +129,26 @@ public class CommandDefault {
 							guild.kickVoiceMember(memberVoiceChannel).queue();
 							fieldTitle = new String[]{"/classe join @[nom du prof]"};
 							fieldContent = new String[]{"Vous permet de rejoindre la classe de votre professeur."};
-							sendPrivateMessage(memberVoiceChannel.getUser(), messageBuilder("Vous avez Ã©tÃ© dÃ©connectÃ©", "Une classe a Ã©tÃ© lancÃ©e dans ce salon "+classe.getVoiceChannel().getName()
+							sendPrivateMessage(memberVoiceChannel.getUser(), messageBuilder("Vous avez été déconnecté", "Une classe a été lancée dans ce salon "+classe.getVoiceChannel().getName()
 									+ "\nPour rejoindre le cours utilisez la commande :",1, fieldTitle, fieldContent,
 									imgError));
 						}
 					}else {
 						fieldTitle = new String[]{"/classe join @[nom du prof]"};
 						fieldContent = new String[]{"Vous permet de rejoindre la classe de votre professeur."};
-						if(!classe.getUsers().contains(memberVoiceChannel.getUser())) sendPrivateMessage(memberVoiceChannel.getUser(), messageBuilder("Merci de vous dÃ©connecter "+memberVoiceChannel.getUser().getAsTag(),
-								"Un cours a dÃ©butÃ© dans le salon "+
-						classe.getVoiceChannel().getName()+"\nPour ne pas le perturber, merci de vous dÃ©connecter. \n"
+						if(!classe.getUsers().contains(memberVoiceChannel.getUser())) sendPrivateMessage(memberVoiceChannel.getUser(), messageBuilder("Merci de vous déconnecter "+memberVoiceChannel.getUser().getAsTag(),
+								"Un cours a débuté dans le salon "+
+						classe.getVoiceChannel().getName()+"\nPour ne pas le perturber, merci de vous déconnecter. \n"
 								+ "Pour rejoindre le cours utilisez la commande :", 1, fieldTitle, fieldContent
 								,imgError));
 					}
 				}
-				fieldTitle = new String[]{"Pour arrÃªter la classe, "+ classe.getProf().getName()+" faites : "};
+				fieldTitle = new String[]{"Pour arrêter la classe, "+ classe.getProf().getName()+" faites : "};
 				fieldContent = new String[]{"/classe stop"};
 				classe.getTextChannel().sendMessage(guild.getRolesByName("@everyone", true).get(0).getAsMention()).queue();
-				classe.getTextChannel().sendMessage(messageBuilder("Classe crÃ©Ã©e ! ", "Tous les Ã©lÃ¨ves souhaiant rejoindre la classe __**" 
+				classe.getTextChannel().sendMessage(messageBuilder("Classe créée ! ", "Tous les élèves souhaiant rejoindre la classe __**" 
 						+classe.getName().toUpperCase()+"**__ de "+classe.getProf().getAsMention() +" dans le salon vocal __**"+classe.getVoiceChannel().getName() +
-						"**__ doivent Ã©crire /classe join "+classe.getProf().getAsMention()+
+						"**__ doivent écrire /classe join "+classe.getProf().getAsMention()+
 						" dans " + classe.getTextChannel().getAsMention() + "et se connecter dans le salon vocal " +classe.getVoiceChannel().getName(),1,fieldTitle,fieldContent,
 						imgLivre)).queue();
 				return;
@@ -156,7 +156,7 @@ public class CommandDefault {
 				if(!hasPermission) {
 					fieldTitle = new String[]{"/classe [help/start/stop/join/quit] [name]"};
 					fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez Ãªtre le prof pour arrÃªter un cours "+ user.getAsMention(),
+					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez être le prof pour arrêter un cours "+ user.getAsMention(),
 							1,fieldTitle,fieldContent,imgError)).queue();
 					return;
 				}
@@ -174,7 +174,7 @@ public class CommandDefault {
 						}
 
 						ClassBot.getClasses().remove(cl);
-						textChannel.sendMessage(messageBuilder("Classe terminÃ©e !", "La classe __**"+cl.getName().toUpperCase()+"**__ de"+ user.getAsMention()+" est maintenant terminÃ©e ! "
+						textChannel.sendMessage(messageBuilder("Classe terminée !", "La classe __**"+cl.getName().toUpperCase()+"**__ de"+ user.getAsMention()+" est maintenant terminée ! "
 								+ "\nN'oubliez pas de faire vos devoirs !",
 								0,null,null,imgLivre)).queue();
 						return;
@@ -183,7 +183,7 @@ public class CommandDefault {
 				break;
 			case "join":
 				if(args.length < 3) {
-					sendPrivateMessage(member.getUser(), messageBuilder("Vous devez prÃ©ciser le nom du prof !", 
+					sendPrivateMessage(member.getUser(), messageBuilder("Vous devez préciser le nom du prof !", 
 							"/classe join @[nomduprof]",0, null, null,
 							imgError));
 					return;
@@ -191,7 +191,7 @@ public class CommandDefault {
 				if(ClassBot.getMemberClasses().containsKey(member)) {
 					fieldTitle = new String[]{"/classe quit"};
 					fieldContent = new String[]{"Vous permet de quitter la classe de votre professeur."};
-					textChannel.sendMessage(messageBuilder("Vous faÃ®tes dÃ©jÃ  parti d'une classe", "Vous devez quitter votre ancienne classe avant de pouvoir en rejoindre une autre "
+					textChannel.sendMessage(messageBuilder("Vous faîtes déjà parti d'une classe", "Vous devez quitter votre ancienne classe avant de pouvoir en rejoindre une autre "
 					+ "\nPour quitter le cours utilisez la commande :",1, fieldTitle, fieldContent,
 						imgError)).queue();
 					return;
@@ -206,8 +206,8 @@ public class CommandDefault {
 						return;
 					}
 				}
-				textChannel.sendMessage(messageBuilder("Erreur", "Le nom prÃ©cisÃ© est incorrect. VÃ©rifiez le nom et recommencez."
-						+ "\n Courage vous pourrez bientÃ´t travailler :wink:",0, null,null,
+				textChannel.sendMessage(messageBuilder("Erreur", "Le nom précisé est incorrect. Vérifiez le nom et recommencez."
+						+ "\n Courage vous pourrez bientôt travailler :wink:",0, null,null,
 						imgError)).queue();
 				return;
 
@@ -231,12 +231,12 @@ public class CommandDefault {
 					}
 					ClassBot.getMemberClasses().get(member).removeUser(member.getUser());
 					ClassBot.getMemberClasses().remove(member);
-					textChannel.sendMessage(messageBuilder(user.getName()+" a quittÃ© la classe!","Est-ce un dÃ©serteur ? Il n'aimait plus le doux son de la voix de "+cl.getProf().getAsMention()+" ?"
+					textChannel.sendMessage(messageBuilder(user.getName()+" a quitté la classe!","Est-ce un déserteur ? Il n'aimait plus le doux son de la voix de "+cl.getProf().getAsMention()+" ?"
 					+"\n Les autres, retournez au travail !",0,null,null,imgLivre)).queue();
 					return;
 				} else {
-					sendPrivateMessage(member.getUser(), messageBuilder("Vous ne devez pas Ãªtre le prof !", 
-							"Vous ne pouvez pas quitter la classe si vous Ãªtes le prof. Vous pouvez cependant la terminer avec /classe stop!",0, null, null,
+					sendPrivateMessage(member.getUser(), messageBuilder("Vous ne devez pas être le prof !", 
+							"Vous ne pouvez pas quitter la classe si vous êtes le prof. Vous pouvez cependant la terminer avec /classe stop!",0, null, null,
 							imgError));
 					return;
 				}
@@ -244,7 +244,7 @@ public class CommandDefault {
 				if(!hasPermission) {
 					fieldTitle = new String[]{"/classe [help/start [name]/stop/join @[nomduProf]/quit/mute/info {nom}]"};
 					fieldContent = new String[]{"Permet d'effectuer des actions sur la classe"};
-					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez Ãªtre le prof pour rendre muet la classe "+ user.getAsMention(),
+					textChannel.sendMessage(messageBuilder("Erreur", "Vous devez être le prof pour rendre muet la classe "+ user.getAsMention(),
 							1,fieldTitle,fieldContent,imgError)).queue();
 					return;
 				}
@@ -259,14 +259,14 @@ public class CommandDefault {
 						cls.setMuted(!mute);
 						if(!mute) {
 						String[] fieldTitle1 = {"/question [sujet]"};
-						String[] fieldContent1 = {"Vous permet de demander Ã  prendre la parole."};
-						textChannel.sendMessage(messageBuilder("La classe Ã  Ã©tÃ© rendue muette", "Pour participer vocalement, demandez l'autorisation Ã  "
+						String[] fieldContent1 = {"Vous permet de demander à prendre la parole."};
+						textChannel.sendMessage(messageBuilder("La classe a été rendue muette", "Pour participer vocalement, demandez l'autorisation à "
 						+cls.getProf().getAsMention()+" avec la commande"
 								,1,fieldTitle1,fieldContent1,imgLivre)).queue();
 						return;
 						}else {
-							textChannel.sendMessage(messageBuilder("La parole Ã  Ã©tÃ© rendue Ã  la plÃ¨be", cls.getProf().getAsMention() +" vous autorise, Ãªtres insignifiants, Ã  parler sans modÃ©ration."
-									+ "\n Vous Ã©coutera-t-il ? C'est une autre question.."
+							textChannel.sendMessage(messageBuilder("La parole à été rendue à la plèbe", cls.getProf().getAsMention() +" vous autorise, êtres insignifiants, à parler sans modération."
+									+ "\n Vous écoutera-t-il ? C'est une autre question.."
 									,0,null,null,imgLivre)).queue();
 							return;
 						}
@@ -277,11 +277,11 @@ public class CommandDefault {
 			
 			default:
 				fieldTitle = new String[]{"/classe help","/classe start [name]","/classe join @[nom du prof]","/classe stop","/classe quit","/classe mute","/question [question]"};
-				fieldContent = new String[]{"Ouvre cette interface","Permet de crÃ©er une classe si vous Ãªtes prof. Vous devez Ãªtre connectÃ© dans un salon vocal et textuel oÃ¹ aucune classe n'est lancÃ©e,"
-						+ " et prÃ©ciser un nom de classe unique.", "Permet aux Ã©lÃ¨ves de rejoindre la classe de leur professeur. Ils doivent se connecter dans le salon vocal oÃ¹ la classe est lancÃ©e.",
-						"Permet aux professeurs de terminer leur classe. NÃ©cÃ©ssaire pour en dÃ©marrer une nouvelle.","Permet aux Ã©lÃ¨ves de quitter la classe","Rend muet ou rend la parole aux Ã©lÃ¨ves" ,
-						"Permet aux Ã©lÃ¨ves d'une classe de demander la parole au professeur pour pouvoir poser une question."};
-				textChannel.sendMessage(messageBuilder("Aide", "Liste des diffÃ©rentes commandes :",7,fieldTitle,fieldContent,
+				fieldContent = new String[]{"Ouvre cette interface","Permet de créer une classe si vous êtes prof. Vous devez être connecté dans un salon vocal et textuel oÃ¹ aucune classe n'est lancée,"
+						+ " et préciser un nom de classe unique.", "Permet aux élèves de rejoindre la classe de leur professeur. Ils doivent se connecter dans le salon vocal oÃ¹ la classe est lancée.",
+						"Permet aux professeurs de terminer leur classe. Nécéssaire pour en démarrer une nouvelle.","Permet aux élèves de quitter la classe","Rend muet ou rend la parole aux élèves" ,
+						"Permet aux élèves d'une classe de demander la parole au professeur pour pouvoir poser une question."};
+				textChannel.sendMessage(messageBuilder("Aide", "Liste des différentes commandes :",7,fieldTitle,fieldContent,
 						imgLivre)).queue();
 				return;
 		}
@@ -301,24 +301,24 @@ public class CommandDefault {
 							if(cl.getQuestionByMember(member) == null) {
 								cl.addQuestion(question);
 								textChannel.sendMessage(messageBuilder(user.getName()+" a une question !",cl.getProf().getAsMention()+" "+subject+" "
-										+ "\n Pour l'autoriser Ã  parler, cliquez sur : :white_check_mark: sinon, cliquez sur : :x: \n Pour supprimer votre question "+user.getAsMention()+" cliquez sur :x:"
+										+ "\n Pour l'autoriser à parler, cliquez sur : :white_check_mark: sinon, cliquez sur : :x: \n Pour supprimer votre question "+user.getAsMention()+" cliquez sur :x:"
 										+ "\n si vous souhaitez le faire taire cliquez sur la :x:",
 										0,null,null,imgLivre)).queue(message -> {
-											message.addReaction("âœ…").queue();
-											message.addReaction("â�Œ").queue();
+											message.addReaction("✅").queue();
+											message.addReaction("❌").queue();
 											cl.getQuestionByMember(member).setMessage(message);
 										});
 							}else {
 								String[] fieldTitle = new String[]{"/question [question]"};
 								String[] fieldContent = new String[]{"Vous permet de poser une question."};
 								sendPrivateMessage(member.getUser(), messageBuilder("Vous ne pouvez posez qu'une seule question", 
-										"Afin de pouvoir poser une nouvelle question, merci de supprimer l'ancienne en cliquant sur la :x: situÃ© sous le message de question",1, fieldTitle, fieldContent, imgError));
+										"Afin de pouvoir poser une nouvelle question, merci de supprimer l'ancienne en cliquant sur la :x: située sous le message de question",1, fieldTitle, fieldContent, imgError));
 							}
 						}else {
 							String[] fieldTitle = new String[]{"/question [question]"};
 							String[] fieldContent = new String[]{"Vous permet de poser une question."};
-							sendPrivateMessage(member.getUser(), messageBuilder("Merci de prÃ©ciser le sujet de votre question", 
-									"Afin de pouvoir poser une question, vous devez prÃ©ciser son sujet !",1, fieldTitle, fieldContent,
+							sendPrivateMessage(member.getUser(), messageBuilder("Merci de préciser le sujet de votre question", 
+									"Afin de pouvoir poser une question, vous devez préciser son sujet !",1, fieldTitle, fieldContent,
 									imgError));
 						}
 					}else {
@@ -338,8 +338,8 @@ public class CommandDefault {
 			}else {
 				String[] fieldTitle = new String[]{"/question [question]"};
 				String[] fieldContent = new String[]{"Vous permet de poser une question."};
-				sendPrivateMessage(member.getUser(), messageBuilder("Vous ne devez pas Ãªtre le prof", 
-						"Afin de pouvoir poser une question, vous ne devez pas Ãªtre le prof !",1, fieldTitle, fieldContent,
+				sendPrivateMessage(member.getUser(), messageBuilder("Vous ne devez pas être le prof", 
+						"Afin de pouvoir poser une question, vous ne devez pas être le prof !",1, fieldTitle, fieldContent,
 						imgError));
 			}
 		}else {
@@ -367,7 +367,7 @@ public class CommandDefault {
 			}
 		}
 
-		eb.setFooter("ClasseBot -- Â©Valentin Lemaire -- Credits: JÃ©rÃ´me LÃ©cuyer");
+		eb.setFooter("ClasseBot -- ©Valentin Lemaire -- Credits: Jérôme Lécuyer");
 		eb.setThumbnail(thumb);
 		MessageEmbed message = eb.build();
 		return message;
